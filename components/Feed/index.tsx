93c73ed769a1fb96179e03dc5c7131167e5ca226
madrigal1 <kurupgokul11@gmail.com>
2020-11-12 23:58:18 +0530

    fix: margin

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -4,7 +4,7 @@ import posts from "../../fetchRequests/feed";
 import Post from "../Post";
 
 const Feed = () => (
-  <View style={{ width: "100%" }}>
+  <View style={{ width: "100%",marginTop:25 }}>
     <FlatList
       data={posts}
       renderItem={({ item }) => <Post post={item} />}
