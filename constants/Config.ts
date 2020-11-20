02cd53248d7b865ac00862dd37fc022708a4f582
madrigal1 <kurupgokul11@gmail.com>
2020-11-20 12:58:53 +0530

    fix: keys

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,4 +1,4 @@
-export const androidClientId = "334320953247-hlvp0dom4i7ifhqtl0mb2mpq6phir509.apps.googleusercontent.com";
+export const androidClientId = "334320953247-l6iu69k9146bbfjol53lside2vlj2o8r.apps.googleusercontent.com";
 export const discordClientId = "761818497201209374";
 export const discordClientSecret = "plFbmQpnByM8oSBfIfp475ZWyhzzltCW";
 export const discordOauthLink="https://discord.com/api/oauth2/authorize?client_id=761818497201209374&redirect_uri=https%3A%2F%2Fauth.expo.io%2F%40madrigal1%2Facminternalapprn&response_type=code&scope=identify%20email%20guilds"
