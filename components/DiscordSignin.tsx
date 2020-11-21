6df6733c92002b9a9ffccba570dae25442409e2b
madrigal1 <kurupgokul11@gmail.com>
2020-11-21 11:33:14 +0530

    fix:clousure fixes on login function

--- components/DiscordSignin.tsx
+++ components/DiscordSignin.tsx
@@ -16,75 +16,91 @@ export type DiscordSigninParams = {
     setDiscordSignin:GenericFunc,
     discordSigninStatus:GenericFunc 
 }
+export type LoginParams = {
+  // tokens:{
+  //   accessToken:string,
+  //   refreshToken:string
+  // },
+  globalState:IGlobalState,
+  setDiscordSignin:GenericFunc,
+  setGlobalState:GenericFunc,
+  discordSigninStatus:GenericFunc
+}
+const handleLogin = async ({globalState,setDiscordSignin,setGlobalState,discordSigninStatus}:LoginParams) => {
+  console.log("inside function: ",globalState);
+  const response = await AuthSession.startAsync({
+    authUrl: discordOauthLink,
+  });
+  //@ts-ignore
+  if(!response.params.code) {
+    console.log("Auth Failed. Curse the devs");
+    return;
+  }
+  const data = new FormData();
+  data.append('client_id',discordClientId);
+  data.append('client_secret',discordClientSecret);
+  data.append('grant_type','authorization_code');
+    //@ts-ignore
+  data.append('code',response.params.code);
+  data.append('redirect_uri',redirectUri);
+  data.append('scope','identify email guilds');
+
+  const accessTokenRequest = await fetch(`${discordApi}/oauth2/token`,{
+    method:'POST',
+    body:data,
+    headers:{
+      'Content-Type': 'multipart/form-data',
+    }
+  })
+  const accessTokenResponse = await accessTokenRequest.json();
+  if(!accessTokenResponse.access_token)
+   console.log("error in accesstoken");
+  const userRequest = await fetch(`${discordApi}/users/@me`,{
+    headers:{
+      'Authorization': `Bearer ${accessTokenResponse.access_token}`,
+      "Content-Type": "application/x-www-form-urlencoded" 
+    }
+  })
+  const user = await userRequest.json();
+  console.log("the tokens y1: ",globalState.tokens);
+  if(!globalState.tokens) {
+    console.log("nope,gess nt error beep boop",globalState.tokens);
+    return;
+  }
+  const addDiscordReq = await fetch(`${baseUrl}/v1/access/login/discord`,{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      'Authorization': `Bearer ${globalState.tokens.accessToken}`,
+      'discord_token': `Bearer ${accessTokenResponse.access_token}`,
+    }
+  })
+  const addDiscordRes = await addDiscordReq.json();
+  console.log("discord Response: ",JSON.stringify(addDiscordRes));
+  if(!user) {
+    console.warn("Auth failed");
+    console.log("Auth failed");
+  }
+  setDiscordSignin({
+    signedIn:true,
+    username:user.username,
+  });
+  discordSigninStatus(true);
+  setGlobalState((globalState:any) => ({...globalState, discordUser:user}));
+}
+
+interface tokens  {
+  accessToken:string,
+  refreshToken:string,
+}
 
 export default function DiscordSignin({setDiscordSignin,discordSigninStatus}:DiscordSigninParams) {
   const [name, setName] = React.useState(null);
   const [globalState,setGlobalState] = React.useContext(GlobalState);
-
-  const handleLogin = async () => {
-    const response = await AuthSession.startAsync({
-      authUrl: discordOauthLink,
-    });
-    //@ts-ignore
-    if(!response.params.code) {
-      console.log("Auth Failed. Curse the devs");
-      return;
-    }
-    const data = new FormData();
-    data.append('client_id',discordClientId);
-    data.append('client_secret',discordClientSecret);
-    data.append('grant_type','authorization_code');
-      //@ts-ignore
-    data.append('code',response.params.code);
-    data.append('redirect_uri',redirectUri);
-    data.append('scope','identify email guilds');
-
-    const accessTokenRequest = await fetch(`${discordApi}/oauth2/token`,{
-      method:'POST',
-      body:data,
-      headers:{
-        'Content-Type': 'multipart/form-data',
-      }
-    })
-    const accessTokenResponse = await accessTokenRequest.json();
-    if(!accessTokenResponse.access_token)
-     console.log("error in accesstoken");
-    const userRequest = await fetch(`${discordApi}/users/@me`,{
-      headers:{
-        'Authorization': `Bearer ${accessTokenResponse.access_token}`,
-        "Content-Type": "application/x-www-form-urlencoded" 
-      }
-    })
-    const user = await userRequest.json();
-    let tokens = globalState.tokens;
-    if(!tokens?.accessToken) {
-      console.warn("error in getting tokens");
-      return;
-    }
-    const addDiscordReq = await fetch(`${baseUrl}/v1/access/login/discord`,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
-        'Authorization': `Bearer ${tokens.accessToken}`,
-        'discord_token': `Bearer ${accessTokenResponse.access_token}`,
-      }
-    })
-    const addDiscordRes = await addDiscordReq.json();
-    if(!user) {
-      console.warn("Auth failed");
-      console.log("Auth failed");
-    }
-    setDiscordSignin({
-      signedIn:true,
-      username:user.username,
-    });
-    discordSigninStatus(true);
-    setGlobalState((globalState:any) => ({...globalState, discordUser:user}));
-}
+  const [tokens,setTokens] = React.useState<tokens>({accessToken:"",refreshToken:""});
 React.useEffect(()=>{
-  console.log("globalState Finalac:",globalState)
-},[globalState]);
-
+},[])
+  
   return (
     <View style={styles.container}>
       {name ? (
@@ -93,7 +109,7 @@ React.useEffect(()=>{
         <View style={styles.button}>
             <Button
           title="Discord Login"
-          onPress={() => handleLogin()}
+          onPress={() =>{ handleLogin({globalState,setDiscordSignin,setGlobalState,discordSigninStatus})}}
         />
         </View>      
       )}
